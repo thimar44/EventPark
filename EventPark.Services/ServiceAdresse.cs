d3f62356d8e5c8e0ab40bd604b2d6782ba0349b8
tmarcoux2017 <tmarcoux2017@22-UC34-09>
2018-06-01 11:48:52 +0200

    fix_bug

--- EventPark.Services/ServiceAdresse.cs
+++ EventPark.Services/ServiceAdresse.cs
@@ -44,16 +44,28 @@ namespace EventPark.Services
 
         public static void Insert(Adresse a)
         {
+#pragma warning disable CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             using (EparkContext context = new EparkContext())
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning restore CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
             {
                 context.Adresses.Add(a);
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning disable CS1061 // 'EparkContext' ne contient pas de définition pour 'SaveChanges' et aucune méthode d'extension 'SaveChanges' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
                 context.SaveChanges();
+#pragma warning restore CS1061 // 'EparkContext' ne contient pas de définition pour 'SaveChanges' et aucune méthode d'extension 'SaveChanges' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             }
         }
 
         public static void Update(Adresse a)
         {
+#pragma warning disable CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             using (EparkContext context = new EparkContext())
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning restore CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
             {
                 EntityState s = context.Entry(a).State;
                 Adresse aExistant = Get(a.id, context);
--- EventPark.Services/ServiceEvenement.cs
+++ EventPark.Services/ServiceEvenement.cs
@@ -14,7 +14,11 @@ namespace EventPark.Services
         public static List<Evenement> GetAll()
         {
             List<Evenement> retour = null;
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning disable CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
             using (EparkContext context = new EparkContext())
+#pragma warning restore CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             {
                 retour = context.Evenements.ToList();
 
@@ -27,7 +31,11 @@ namespace EventPark.Services
         public static Evenement Get(Guid id)
         {
             Evenement retour = null;
+#pragma warning disable CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             using (EparkContext context = new EparkContext())
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning restore CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
             {
                 retour = Get(id, context);
 
@@ -43,18 +51,34 @@ namespace EventPark.Services
 
         public static void Insert(Evenement e)
         {
+#pragma warning disable CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             using (EparkContext context = new EparkContext())
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning restore CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
             {
                 context.Evenements.Add(e);
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning disable CS1061 // 'EparkContext' ne contient pas de définition pour 'SaveChanges' et aucune méthode d'extension 'SaveChanges' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
                 context.SaveChanges();
+#pragma warning restore CS1061 // 'EparkContext' ne contient pas de définition pour 'SaveChanges' et aucune méthode d'extension 'SaveChanges' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             }
         }
 
         public static void Update(Evenement e)
         {
+#pragma warning disable CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             using (EparkContext context = new EparkContext())
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning restore CS1674 // 'EparkContext' : le type utilisé dans une instruction using doit être implicitement convertible en 'System.IDisposable'
             {
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning disable CS1061 // 'EparkContext' ne contient pas de définition pour 'Entry' et aucune méthode d'extension 'Entry' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
                 EntityState s = context.Entry(e).State;
+#pragma warning restore CS1061 // 'EparkContext' ne contient pas de définition pour 'Entry' et aucune méthode d'extension 'Entry' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
                 Evenement eExistant = Get(e.id, context);
                 eExistant.theme = e.theme;
                 eExistant.date = e.date;
@@ -66,7 +90,11 @@ namespace EventPark.Services
                 eExistant.images = e.images;
                 eExistant.titre = e.titre;
 
+#pragma warning disable CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
+#pragma warning disable CS1061 // 'EparkContext' ne contient pas de définition pour 'SaveChanges' et aucune méthode d'extension 'SaveChanges' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
                 context.SaveChanges();
+#pragma warning restore CS1061 // 'EparkContext' ne contient pas de définition pour 'SaveChanges' et aucune méthode d'extension 'SaveChanges' acceptant un premier argument de type 'EparkContext' n'a été trouvée (une directive using ou une référence d'assembly est-elle manquante ?)
+#pragma warning restore CS0012 // Le type 'IdentityDbContext<>' est défini dans un assembly qui n'est pas référencé. Vous devez ajouter une référence à l'assembly 'Microsoft.AspNet.Identity.EntityFramework, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'.
             }
         }
     }
